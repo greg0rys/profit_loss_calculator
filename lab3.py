3b7010ef76c62a394308fa659ea67c49f31b3b81
Greg <60129025+greg0rys@users.noreply.github.com>
2021-05-02 00:19:06 -0700

    lossPercent(a,b)
    Created this function to take the total loss and convert it into a percentage.

--- lab3.py
+++ lab3.py
@@ -1,9 +1,12 @@
 # define the getValue() module 
+# Which will get the total of the invoice from the user
 def getValue():
     purchaseValue = float(input('Please enter the invoice total: '))
     return purchaseValue
 
 #define the itemValue module
+# Using a user controlled for loop and an accumulator we will collect the cost of each item
+# and return their total 
 def itemValue():
     itemVal = 0.00
     numItems = int(input('How many items would you like to markdown?'))
@@ -13,10 +16,17 @@ def itemValue():
         cost = float(input('Please enter the items cost: $'))   
         itemVal = itemVal + cost
     return itemVal
-# Declare the redeuced by module to calculate the sum in dollars
+# Declare the redeuced by module to calculate the sum of the markdowns
 def redeucedby(x,y):
     sum = int(x - y)
     return sum
+
+# declare the lossPercent() which will take the shipmentValue and 
+# markdownAmount as arguments to conver the loss to a percentage
+
+def lossPercent(a,b):
+    perHundred = (b/a) * 100
+    return perHundred
 
 # declare the main()
 
@@ -24,5 +34,8 @@ def main():
     shipmentValue = getValue()
     itemCost = itemValue()
     markdownAmount = int(redeucedby(shipmentValue,itemCost))
-    print('You spent: $', shipmentValue, '\n You did a total markdown for: $',itemCost)
+    markdownPercentage = lossPercent(shipmentValue,markdownAmount)
+    print('You spent: $', shipmentValue)
+    print('You did a total markdown for: $',itemCost)
+    print('Which is:',int(markdownPercentage),'%')
 main()
