5fa7853c604540a568c2078c357cac4c34648efc
Greg <60129025+greg0rys@users.noreply.github.com>
2021-05-01 23:58:03 -0700

    Update lab3.py
    updated shipment_value to purchaseValue
    
    line 8 - I added an accumulator variable to hold the total for the items that have been marked down
    
    line 12-18:
    Removed lines of code that weren't neccssary
    
    Declared a main()
    to call the functions and output their data

--- lab3.py
+++ lab3.py
@@ -1,21 +1,28 @@
 # define the getValue() module 
 def getValue():
-    shipment_value = float(input('Please enter the invoice total: '))
-    return shipment_value
+    purchaseValue = float(input('Please enter the invoice total: '))
+    return purchaseValue
 
 #define the itemValue module
 def itemValue():
+    itemVal = 0.00
     numItems = int(input('How many items would you like to markdown?'))
 
     #Run a for loop controled by the input of numItems
     for counter in range(1,numItems + 1):
-        cost = float(input('Please enter the items cost \t : $'))   
-        itemVal = cost
-        #declare an accumulator variable to hold the running total
-        # we can return the running total
-        runningTotal = itemVal
-        print('Item: ' +str(counter) + '\t' 'Value: ' + str(itemVal))
-    print('your running total is ', runningTotal)
+        cost = float(input('Please enter the items cost: $'))   
+        itemVal = itemVal + cost
+    return itemVal
+# Declare the redeuced by module to calculate the sum in dollars
+def redeucedby(x,y):
+    sum = int(x - y)
+    return sum
 
-getValue()
-itemValue()
+# declare the main()
+
+def main():
+    shipmentValue = getValue()
+    itemCost = itemValue()
+    markdownAmount = int(redeucedby(shipmentValue,itemCost))
+    print('You spent: $', shipmentValue, '\n You did a total markdown for: $',itemCost)
+main()
