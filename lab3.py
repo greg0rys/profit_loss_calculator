20ec7655b68f2003a52ebf0e77ab42a5515ee6d8
Greg <60129025+greg0rys@users.noreply.github.com>
2021-05-02 23:17:47 -0700

    Added required header information Lab info for class Pseudocode for the overall program

--- lab3.py
+++ lab3.py
@@ -1,3 +1,144 @@
+# *******************************************************************************
+# Author: Gregory Shenefelt
+# Lab: Lab 3
+# Date: 04/21/2021
+# Description: A program that helps you see how much you lost on a shipment \n
+#  due to markdowns
+# Input: The invoice total, the numbers of items to markdown, and the amount \n
+#  of dollars the item has been redeuced by
+# Output: The total of the shipment, how many dollars it was redeuced by \n
+# and the percentage of loss.
+# Sources: course materials, gaddis book.
+
+
+# Pseudocode:
+# // This program will tell the user how much they have lost in a shipment 
+# // Due to markdowns. 
+# // It will then display to the user the amount they have lost in 
+# // both dollars and %
+# // It will then output a message to the user
+# // it will also 
+# // Input List: shipmentValue – the total cost of the shipment,
+# // itemsCost – the cost of the item
+# // markdownAmount – amount the item has been marked down in dollars
+# // Output List: shipmentValue //dollarsLost – the amount of dollars lost // firstRun – an optional message if this is their first time using the program
+# // lossPercentage – the total amount of the loss as a percent 
+# // A message to the user showing the original cost of the shipment
+# // The amount of dollars lost // and what percentage of the shipmentValue
+# // was lost
+
+# //newUserMsg() asks the user if this is their first time running the program
+# // if the user enters y it will let the user know how this program works
+# // if the user enters n it will just tell the user to proceed 
+# Module newUserMsg()
+# 	// Local Variable
+# 	Declare String msg 
+# 	Display “Is this the first time using this program? Enter y/n”
+# 	If msg == ‘y’ Then
+# 	Display “Welcome to your first run”
+#   Display “I will ask for your invoice total, number of  markdowns, and the sum to reduce by”
+#   Display “I will then calculate your loss in dollars and percent”
+#   Display “And advise you if you should reorder them or not”
+#   Else
+# 	Display “Please begin entering your values”
+#   End If
+# End Module
+
+# // getValue() gets the total cost of the shipment and returns it
+# Module getValue()
+# 	// Local Variable
+# 	Declare Real shipmentValue 
+# 	Display “Enter the total from the invoice:”
+# 	Input shipmentValue
+# // Return the shipment value to the main module
+# Return shipmentValue
+# End Module
+
+# // the itemValue() asks the user to input the number of items that
+# // need to be marked down and collects the cost of the item
+# // and then returns it
+
+# Module itemValue()
+# 	// Declare local variables 
+# 	// these values will allow the user to control the amount of times
+# // the loop will iterate.
+#   Declare Integer, numItems
+#    Display “How many items would you like to markdown?”
+#   Input numItems 
+#   // Enter the user controlled for loop that will iterate from 1 to numItems + 1 // so that way we capture all the items the user wishes to markdown
+#   For counter = 1, numItems +1
+#   // Declare a variable to hold the cost of the item
+#   Declare Real itemVal = 0.0
+#   Declare Real cost
+#   Display “Enter the cost of item”,counter
+#   Input cost
+#   Set itemVal = itemVal + cost
+# End For
+# // Return the total amount of the markdowns to the main module
+# Return itemVal
+# End Module
+	
+# // The reducedby() will take the itemCost and the shipmentValue as arguments and subtract the two then it will return the product to the markdownAmount variable	
+# Module reducedby(Declare Real x, Declare Real y)
+# 	// define a local variable to hold the calculations sum
+# 	Declare Real sum
+# 	// x will hold the shipmentValue and y will hold the itemCost
+# 	Set sum = x – y
+# // return the sum back to the main module 
+# Return sum
+# End Module
+
+# // the lossPercent() will take the shipmentValue and itemCost as arguments and convert it to a percent
+# Module lossPercent(declare Real a, Real b)
+# 	// Declare the local variables that will perform a calculation
+# 	Declare Real perHundred 
+# 	// calculate the percentage of the shipment that was lost due to markdowns
+# 	perHundred = (b/a) * 100
+# Return perHundred
+# End module
+
+# // the lossAlert(real z) module will take the itemCost as an argument and depending on the amount will output a message
+# Module lossAlert(real z)
+# 	// Determine the message to output to the user
+# 	If z > 100 Then
+# 		Display “High losses reported, please review order flow”
+# 	Else If z > 35 Then
+# 		Display “Please don’t order anymore”
+# 	Else
+# 		Display “Low losses reported”
+# 		Display “Please track sales on items”
+# 		Display “to determine if you should keep them on hand”
+# 	End If
+# // Return the message back to the main module
+# Return z
+# End Module
+
+# // The main() will call all the modules and run them and output 
+# // The total spent, the total dollars lost, and the percent lost
+
+# Module main()
+# 	// Local Variables
+# 	Declare String firstRun, message
+# Declare Real shipmentValue,itemCost,markdownAmount
+# 	Declare Real markdownPercent, 
+# 	Call newUserMsg
+# 	Set firstRun
+# 	Call getValue()
+# 	Set shipmentValue
+# 	Call redeucedby(shipmentCost,itemCost)
+# 	Set markdownAmount
+# 	Call lossPercent(shipmentValue,itemCost)
+# 	Set markdownPercent
+# 	Call lossAlert(itemCost)
+# 	Set message
+# 	Display “You spent: $”, shipmentValue
+# 	Display “You did a total markdown for: $”,itemCost
+# 	Display “Which is”,markdownPercentage,”%”
+# End Module
+# Call main()
+# *******************************************************************************
+#Begin Python Program 
+
 # define newUserMsg() which will ask the user if this is their first time launching this program
 # it will then use an if statement to determine if it should outprint what it does
 # to the user
